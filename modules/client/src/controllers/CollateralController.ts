9263c36ca011d5f972e0f2e8fb61fcf4dd3bdad1
LayneHaber <layne.haber@gmail.com>
2019-05-05 22:46:23 -0700

    remove console.log

--- modules/client/src/controllers/CollateralController.ts
+++ modules/client/src/controllers/CollateralController.ts
@@ -2,9 +2,7 @@ import { AbstractController } from './AbstractController'
 import { getTxCount } from '../state/getters';
 
 export default class CollateralController extends AbstractController {
-
   public requestCollateral = async (): Promise<void> => {
-    console.log(`requestCollateral`)
     const sync = await this.hub.requestCollateral(getTxCount(this.store.getState()))
     this.connext.syncController.handleHubSync(sync)
   }
